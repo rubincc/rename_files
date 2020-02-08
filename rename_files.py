6b70fadc7fb462243790642d8cb81eb220765e60
rubincc <60546362+rubincc@users.noreply.github.com>
2020-02-08 12:29:09 +0000

    Commented debug lines to remove the clutter.

--- rename_files.py
+++ rename_files.py
@@ -43,25 +43,25 @@ def rnd_str(str_length=5):
 for (dirpath, dirnames, filenames) in os.walk(scan_path):
     for f in filenames:
         old_name = os.path.join(dirpath,f)
-        print(f"Short filename is {f}")
+        #print(f"Short filename is {f}")
 
         # test if the filename contains garbage_string
         if garbage_string in old_name:
-            print(f"The file {old_name} contains the string {garbage_string} so will be changed.")
+            #print(f"The file {old_name} contains the string {garbage_string} so will be changed.")
             # subtract the garbage_string from old_name in new_name
             new_name = old_name.replace(garbage_string, '')
-            print(f"New name will be {new_name}.")
+            #print(f"New name will be {new_name}.")
         
             # Test if a file with new_name exist already
             if Path(new_name).is_file():
-                print(f"The file {new_name} already exist and stays unchanged.")
+                #print(f"{new_name} already exist and stays unchanged.")
                 # generate a final_name with a random 5 string at start
                 final_name = os.path.join(dirpath, rnd_str() + "_" + f.replace(garbage_string, ''))
-                print(f"The file {old_name} was renamed as {final_name}")
-                #os.renames(old_name, final_name)
+                print(f"{old_name} was renamed as {final_name}")
+                os.renames(old_name, final_name)
             else:
                 # if a file with name new_name does not exist
                 print(f"{old_name} was renamed {new_name}")
-                #os.renames(old_name, new_name)
+                os.renames(old_name, new_name)
 sys.exit()
           
